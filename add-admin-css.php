b6ac5dd6fa9f12251a71ead5780a79a5f7fe36b0
Scott Reilly <scott@coffee2code.com>
2021-05-11 23:38:35 -0700

    c2c_Plugin: Update version to 062

--- add-admin-css.php
+++ add-admin-css.php
@@ -45,7 +45,7 @@ if ( ! class_exists( 'c2c_AddAdminCSS' ) ) :
 
 require_once( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'c2c-plugin.php' );
 
-final class c2c_AddAdminCSS extends c2c_Plugin_061 {
+final class c2c_AddAdminCSS extends c2c_Plugin_062 {
 
 	/**
 	 * Name of plugin's setting.
--- phpunit/tests/test-add-admin-css.php
+++ phpunit/tests/test-add-admin-css.php
@@ -184,11 +184,11 @@ class Add_Admin_CSS_Test extends WP_UnitTestCase {
 	}
 
 	public function test_plugin_framework_class_name() {
-		$this->assertTrue( class_exists( 'c2c_Plugin_061' ) );
+		$this->assertTrue( class_exists( 'c2c_Plugin_062' ) );
 	}
 
 	public function test_plugin_framework_version() {
-		$this->assertEquals( '061', $this->obj->c2c_plugin_version() );
+		$this->assertEquals( '062', $this->obj->c2c_plugin_version() );
 	}
 
 	public function test_version() {
