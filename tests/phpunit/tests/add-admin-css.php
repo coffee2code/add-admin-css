473ca51fc34aebe5288496496ef24c36141401d0
Scott Reilly <scott@coffee2code.com>
2021-05-30 23:38:39 -0700

    Update to use plugin framework v063

--- tests/phpunit/tests/add-admin-css.php
+++ tests/phpunit/tests/add-admin-css.php
@@ -184,11 +184,11 @@ class Add_Admin_CSS_Test extends WP_UnitTestCase {
 	}
 
 	public function test_plugin_framework_class_name() {
-		$this->assertTrue( class_exists( 'c2c_Plugin_062' ) );
+		$this->assertTrue( class_exists( 'c2c_Plugin_063' ) );
 	}
 
 	public function test_plugin_framework_version() {
-		$this->assertEquals( '062', $this->obj->c2c_plugin_version() );
+		$this->assertEquals( '063', $this->obj->c2c_plugin_version() );
 	}
 
 	public function test_version() {
